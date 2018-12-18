33a96b5c3d4e860e3b0d36e45f4d2d9cef4f3b9d
esc_jmelo <esc_jmelo@192-168-56-1.privateip>
2018-12-18 14:42:44 -0200

    Remoção do ldap para teste

--- src/main/java/br/com/uoldiveo/portal/restcontroller/LoginRestController.java
+++ src/main/java/br/com/uoldiveo/portal/restcontroller/LoginRestController.java
@@ -1,9 +1,5 @@
 package br.com.uoldiveo.portal.restcontroller;
 
-import java.util.List;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.ldap.core.LdapTemplate;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,14 +8,8 @@ import br.com.uoldiveo.portal.login.domain.User;
 @RestController
 public class LoginRestController {
 
-	@Autowired
-    private LdapTemplate ldapTemplate;
-	
 	@RequestMapping(value="/login")
 	public User login() {
-		
-		List<String> list = ldapTemplate.list("OU=Clientes");
-		
 		return new User("esc_jmelo@uoldiveo.com.br", "Jonas Goulart");
 	}
 }
